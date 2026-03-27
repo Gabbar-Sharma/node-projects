ed05884aace4fc533952bbfb84435f1f622ea781
gabbar sharma <gabbarsharma75663@gmail.com>
2026-03-27 08:44:48 +0530

    adding import and export

---
+++ project-1/combo.js
@@ -0,0 +1,5 @@
+const age  = 45;
+
+console.log("This is combo file");
+
+exports.age = age;
--- project-1/server.js
+++ project-1/server.js
@@ -1,5 +1,5 @@
 
-
+const combo = require("./combo")
 // const add = (a, b , callback) =>{
 // const all = a + b
 // console.log(all);
@@ -9,22 +9,24 @@
 //      console.log("This is a callback function");
 // })
 
-const fs = require("fs")
-const os = require("os")
+// const fs = require("fs")
+// const os = require("os")
 
-const data = fs.appendFile("data.txt", "this is data file" , () => {
-console.log("file is created");
-})
+// const data = fs.appendFile("data.txt", "this is data file" , () => {
+// console.log("file is created");
+// })
 
-const user = os.userInfo()
-console.log(user);
+// const user = os.userInfo()
+// console.log(user);
 
-const a = 34;
-const b = 45
-console.log(a-b)
-if(a > b){
-    console.log("a is greater than b");
-}   else{
-    console.log("b is greater than a");
-}   
+// const a = 34;
+// const b = 45
+// console.log(a-b)
+// if(a > b){
+//     console.log("a is greater than b");
+// }   else{
+//     console.log("b is greater than a");
+// }   
 
+console.log("This is server file");
+console.log(combo.age);
